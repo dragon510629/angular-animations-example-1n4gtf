f5b6102bfcca3ae7208bf5e350066c1c67f0f78a
dragon510629 <38198614+dragon510629@users.noreply.github.com>
2023-05-23 22:17:24 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-animations-example-1n4gtf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-animations-example-1n4gtf)
---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import {
+  trigger,
+  transition,
+  animate,
+  style,
+  state,
+} from '@angular/animations';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(150%)', opacity: 0 }),
+        animate(
+          '300ms ease-in',
+          style({ transform: 'translateX(0%)', opacity: 1 })
+        ),
+      ]),
+    ]),
+  ],
+})
+export class AppComponent {
+  name = 'Angular 6';
+  step: number = 1;
+
+  toggle(evt): void {
+    this.step += 1;
+  }
+}
