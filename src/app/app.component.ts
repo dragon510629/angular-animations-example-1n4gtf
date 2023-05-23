a2c2727f39ad3bce7bdffeee574a0955f13773ed
dragon510629 <38198614+dragon510629@users.noreply.github.com>
2023-05-23 22:36:12 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,18 @@ import {
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('slideInOut', [
-      transition(':enter', [
-        style({ transform: 'translateX(150%)', opacity: 0 }),
+      transition(':leave', [
+        style({ transform: 'translateX(0%)', opacity: 1 }),
         animate(
-          '300ms ease-in',
+          '2000ms ease-in',
+          style({ transform: 'translateX(0%)', opacity: 0 })
+        ),
+      ]),
+
+      transition(':enter', [
+        style({ transform: 'translateX(0%)', opacity: 0 }),
+        animate(
+          '2000ms ease-in',
           style({ transform: 'translateX(0%)', opacity: 1 })
         ),
       ]),
